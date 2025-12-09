d7b29837ebd7fb52f56fb229d245055a63fe5001
MatheusBonadio <matheus_lima_bonadio_xd@hotmail.com>
2025-12-09 18:12:50 -0300

    refactor: reestrutura ponto de entrada da aplicação

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__ . '/src/auth/auth.php';
+require_auth();
+
+if (isset($_SESSION['user_type']) && $_SESSION['user_type'] === 'associado') {
+	require_once __DIR__ . '/src/controllers/AssociadoController.php';
+} else {
+	require_once __DIR__ . '/src/controllers/CupomController.php';
+}
+
+?>
